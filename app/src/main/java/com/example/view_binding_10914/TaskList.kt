554531fc71438640677efbf07ffa4d370909267f
dumbgoat <dumbgoat.00@gmail.com>
2022-09-15 10:06:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10914/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10914
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1","Prologue-","Cerita tentang kehidupan seorang mahasiswa semester 5 yang tampak biasa saja"),
+        Task("Chapter 2","On a Rainy Day, In the Classroom","Hal biasa dalam kesehariannya berubah ketika hari hujan itu di lantai 4"),
+        Task("Chapter 3","Unrest, Regret, and What Comes Next","Masalah datang dari segala arah, dari dalam maupun luar"),
+        Task("Chapter 4","Progress, Impatience, and a Silent Determination","Menghadapi siatuasinya, mahasiswa ini mulai berprogress untuk menghadapi keadaan sekitarnya"),
+        Task("Chapter 5","Fin","Ia yang sudah bersatu dengan teman-temannya akhirnya menyelesaikan masalah tersebut")
+    )
+}
